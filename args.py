dbe132383f9d7ffa475975240ef4415ab74bdcf0
AetherEternity <aethereternity@niuitmo.ru>
2018-07-17 05:43:43 +0300

    Добавлен парсинг vlanmap, добавлен gitignore для pycache

--- args.py
+++ args.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 #на вход полный путь к папке с конфигами и картой сети, название файла с картой сети
 import argparse
 import os
+import re
 parser = argparse.ArgumentParser()
 parser.add_argument("-vl", "--vlanmap",type=str, help="file name of vlanmap")
 parser.add_argument("-c","--config",type=str, help="full path of the folder with config[s] and vlanmap")
@@ -18,3 +19,16 @@ if args.vlanmap and args.config:
         print('incorrect vlanmap name!')
 else:
     print("you must load config[s] and vlanmap!")
+
+# let the parse begin!
+vmapf=open(vlanmap,"r")
+vlanmap=vmapf.read()
+vmapf.close()
+vlanpattern=re.compile(': ([0-9,]+)')
+vlanmap=re.findall(vlanpattern,vlanmap)
+critical_area=vlanmap[0].split(',')
+unknown_area=vlanmap[1].split(',')
+trusted_area=vlanmap[2].split(',')
+print(critical_area)
+print(unknown_area)
+print(trusted_area)
