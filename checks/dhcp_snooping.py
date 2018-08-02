a739c2449cb8f68ba461253d996df8d2b839c31a
Nikita <nikitaloginov96@mail.ru>
2018-08-02 12:51:34 +0300

    Changed dchp and arp checks structure, added dhcp and arp result dictionary as return, fix bugs

--- checks/dhcp_snooping.py
+++ checks/dhcp_snooping.py
@@ -2,7 +2,7 @@ import util
 from termcolor import colored
 from statistics import median
 
-def check(global_params,iface_params,vlanmap,allinterf):
+def check(global_params,iface_params,vlanmap,allinterf,result_dict):
 	score=[]
 	enabled=0
 	snooping_vlans=[]
@@ -10,10 +10,12 @@ def check(global_params,iface_params,vlanmap,allinterf):
 	# globals
 	if(global_params['ip']['dhcp_snooping']['active']=='yes'):
 		enabled=1
+		result_dict['ip']['dhcp_snooping']['active'] = [2,'ENABLED']
 		if ('vlans' in global_params['ip']['dhcp_snooping']):
 			snooping_vlans=util.intify(global_params['ip']['dhcp_snooping']['vlans'])
 	else:
-		print (colored('DHCP snooping disabled','red',attrs=['bold']))
+		# print (colored('DHCP snooping disabled','red',attrs=['bold']))
+		result_dict['ip']['dhcp_snooping']['active'] = [0,'DISABLED', 'Turn it on to prevent spoofing attack']
 		score.append(3)
 
 	# ifaces
@@ -27,25 +29,28 @@ def check(global_params,iface_params,vlanmap,allinterf):
 				continue
 			else:
 				mode=iface_snoop['mode']
-				# dirty hack cuz parsing returns lists sometimes
-				if type(mode) is list:
-					mode=mode[0]
 				if(mode=='untrust'):
 
 					if 'limit' in iface_snoop:
-						if(int(iface_snoop['limit'][0])>100):
-							print (colored('DHCP snooping rate limit is too high at interface '+i,'blue',attrs=['bold']))
+						if(int(iface_snoop['limit'])>100):
+							# print (colored('DHCP snooping rate limit is too high at interface '+i,'blue',attrs=['bold']))
+							result_dict['ip']['dhcp_snooping']['limit'] = [0, 'Too high level at interface '+i,
+																		   'Decrease this level to prevent dhcp attack']
 							score.append(1)
 					else:
-						print (colored('No DHCP snooping rate limit at interface '+i,'blue',attrs=['bold']))
+						# print (colored('No DHCP snooping rate limit at interface '+i,'blue',attrs=['bold']))
+						result_dict['ip']['dhcp_snooping']['limit'] = [0, 'No DHCP snooping rate at interface ' + i,
+																	   'Define this level to prevent dhcp attack']
 						score.append(1)
 
 				elif(mode=='trust'):
 					if(vlanmap and iface_vlans):
 						if(set(vlanmap[2]).isdisjoint(iface_vlans)):
-							print (colored('Interface '+i+' set as trusted, but vlanmap is different','yellow',attrs=['bold']))
+							# print (colored('Interface '+i+' set as trusted, but vlanmap is different','yellow',attrs=['bold']))
+							result_dict['ip']['dhcp_snooping']['vlans'] = [1,'Interface '+i+'set as trusted, but vlanmap is different',
+																		   'Check vlans on vlanmap and this interface']
 							score.append(2)
 				else:
 					print('Unknown mode: '+mode)
 
-	return median(score)
+	return median(score), result_dict
