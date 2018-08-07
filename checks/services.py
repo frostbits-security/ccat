974253c0b89bbda2b4c806faf897b6ed3a7392d4
Nikita <nikitaloginov96@mail.ru>
2018-08-08 02:54:01 +0300

    Changed dictionary structure at service option

--- checks/services.py
+++ checks/services.py
@@ -7,35 +7,30 @@
 #
 
 def check(global_params,results_dict):
-# active services section
+# 'should be enable' services section
     if 'password-encryption' in global_params['active_service']:
-        results_dict['active_service']['password-encryption'] = [2, 'ENABLED']
+        results_dict['services']['password-encryption'] = [2, 'ENABLED']
     else:
-        results_dict['active_service']['password-encryption'] = [0, 'DISABLED',
-                                                                 'Turn it on to encrypt passwords']
+        results_dict['services']['password-encryption'] = [0, 'DISABLED', 'Turn it on to encrypt passwords']
     if 'tcp-keepalives-in' in global_params['active_service']:
-        results_dict['active_service']['tcp-keepalives-in']   = [2, 'ENABLED']
+        results_dict['services']['tcp-keepalives-in']   = [2, 'ENABLED']
     else:
-        results_dict['active_service']['tcp-keepalives-in']   = [1, 'DISABLED',
-                                                                 'You may need it to prevent stuck sessions']
-    if 'udp-small-servers' in global_params['active_service']:
-        results_dict['active_service']['udp-small-servers']   = [0, 'ENABLED',
-                                                                 'Turn it off to prevent potential information leak and'
-                                                                 'DOS attack']
-    else:
-        results_dict['active_service']['udp-small-servers']   = [2, 'DISABLED']
-    if 'tcp-small-servers' in global_params['active_service']:
-        results_dict['active_service']['tcp-small-servers']   = [0, 'ENABLED',
-                                                                 'Turn it off to prevent potential information leak and'
-                                                                 'DOS attack']
-    else:
-        results_dict['active_service']['tcp-small-servers']   = [2, 'DISABLED']
+        results_dict['services']['tcp-keepalives-in']   = [1, 'DISABLED', 'You may need it to prevent stuck sessions']
 
-# disable services section
+# 'should be disable' services section
     if 'pad' in global_params['disable_service']:
-        results_dict['disable_service']['pad'] = [2, 'DISABLED']
+        results_dict['services']['pad'] = [2, 'DISABLED']
     else:
-        results_dict['disable_service']['pad'] = [0, 'ENABLED',
-                                                  'Turn it off to prevent potential unauthorized access']
+        results_dict['services']['pad'] = [0, 'ENABLED', 'Turn it off to prevent potential unauthorized access']
+    if 'udp-small-servers' in global_params['active_service']:
+        results_dict['services']['udp-small-servers']   = [0, 'ENABLED', 'Turn it off to prevent potential information'
+                                                                         'leak and DOS attack']
+    else:
+        results_dict['services']['udp-small-servers']   = [2, 'DISABLED']
+    if 'tcp-small-servers' in global_params['active_service']:
+        results_dict['services']['tcp-small-servers']   = [0, 'ENABLED', 'Turn it off to prevent potential information'
+                                                                         'leak and DOS attack']
+    else:
+        results_dict['services']['tcp-small-servers']   = [2, 'DISABLED']
 
     return results_dict
