b9be09d6a6d773e27629e64e8f26e7605bec85bf
khodunken <khodukinant75@gmail.com>
2018-08-09 14:47:47 +0300

    fixed ccat, fixed storm.cdp.dtp,stp checks

--- checks/stp.py
+++ checks/stp.py
@@ -8,20 +8,20 @@
 #           {{'iface1': {'STP': {'portfast': [severity(int), 'message', 'best practice']}, 'iface2':...}}
 #
 
-def check(iface_dct,dct):
-    for each in iface_dct:
-        result={}
-        if 'stp' in iface_dct[each]:
-            if iface_dct[each]['type'] == 'access' and iface_dct[each]['stp'] == 'portfast':
-                result['portfast'] = [2, 'OK', 'Portfast should be enable']
-            elif 'portfast trunk' in iface_dct[each]['stp']:
-                result['portfast'] =[1, 'WARNING', 'Portfast should be enable']
-            else:
-                result['portfast on trunk'] = [1, 'WARNING', 'Portfast is on the trunk mode port']
+def check(iface_dct):
+
+    result={}
+    if 'stp' in iface_dct:
+        if iface_dct['type'] == 'access' and iface_dct['stp'] == 'portfast':
+            result['portfast'] = [2, 'OK', 'A portfast on access port should be enable']
+        elif 'portfast trunk' in iface_dct['stp']:
+            result['portfast'] =[1, 'WARNING', 'A Portfast on trunk port']
         else:
-            result['portfast'] = [0, 'BAD', 'Portfast should be enable']
-        dct[each].update(result)
-    return dct
+            result['portfast on trunk'] = [1, 'WARNING', 'A Portfast on trunk port']
+    else:
+        result['portfast'] = [1, 'DISABLE', 'A portfast on access port should be enable']
+
+    return result
 
 
 
