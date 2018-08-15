9c36ef0fd841d7fd6b8f8e117722f09a6926ee83
khodunken <34400581+khodunken@users.noreply.github.com>
2018-08-15 10:31:45 +0300

    Update __init__.py

--- checks/__init__.py
+++ checks/__init__.py
@@ -1,2 +1,2 @@
 __all__ = ["arp_inspection", "cdp", "console_vty", "dhcp_snooping", "dtp", "ip_global", "mode", "port_security",
-           "services", "storm_control", "stp", "stp_global", "users","port_sec"]
+           "services", "storm_control", "stp", "stp_global", "users","port_security"]
