27c4951e2640a1ae4c809277e76e8c71e322a7d5
khodunken <khodukinant75@gmail.com>
2018-08-16 09:36:27 +0300

    improved cdp check

--- checks/cdp.py
+++ checks/cdp.py
@@ -6,28 +6,26 @@
 #        updated result dictionary
 #           {{'iface1': {'CDP': {'cdp': [severity(int), 'message', 'best practice']}, 'iface2':...}}
 #
+def cdp_check(result,scale,dct):
+    if 'cdp' in iface_dct:
+        if iface_dct['cdp'] == 'no':
+            result['cdp'] = [scale[1], 'DISABLED']
+        else:
+            result['cdp'] = [scale[0], 'ENABLED', 'CDP should not be enable']
+    else:
+        result['cdp'] = [scale[0], 'ENABLED', 'CDP should not be enable']
+    return result
 
 def check(iface_dct, vlanmap_type):
     result = {}
 
 # If this network segment is TRUSTED - enabled cdp is not a red type of threat, it will be colored in orange
     if vlanmap_type == 'TRUSTED':
-        if 'cdp' in iface_dct:
-            if iface_dct['cdp'] == 'no':
-                result['cdp'] = [2, 'DISABLED']
-            else:
-                result['cdp'] = [1, 'ENABLED', 'CDP should not be enable']
-        else:
-            result['cdp'] = [1, 'ENABLED', 'CDP should not be enable']
+        cdp_check(result,[1,2],iface_dct)
 
 # Otherwise if network segment is CRITICAL or UNKNOWN or vlanmap is not defined - enabled cdp is a red type of threat
     else:
-        if 'cdp' in iface_dct:
-            if iface_dct['cdp'] == 'no':
-                result['cdp'] = [2, 'DISABLED']
-            else:
-                result['cdp'] = [0, 'ENABLED', 'CDP should not be enable']
-        else:
-            result['cdp'] = [0, 'ENABLED', 'CDP should not be enable']
+        cdp_check(result, [0, 2], iface_dct)
+
 
     return result
