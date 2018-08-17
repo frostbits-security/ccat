d96755c023d860e526c12b96adf5471a3e5b602d
khodunken <khodukinant75@gmail.com>
2018-08-17 13:20:32 +0300

    fixed key in arp_insp_global check

--- checks/arp_insp_global.py
+++ checks/arp_insp_global.py
@@ -4,14 +4,14 @@ import util
 def check(global_params, result_dict):
     enabled = 0
     snooping_vlans = []
-    result_dict['ip']['ARP inspection'] = {}
+    result_dict['IP options']['ARP inspection'] = {}
 
     # globals
     if (global_params['ip']['arp_inspection']['active'] == 'yes'):
         enabled = 1
-        result_dict['ip']['ARP inspection'] = [2, 'ENABLED']
+        result_dict['IP options']['ARP inspection'] = [2, 'ENABLED']
         if ('vlans' in global_params['ip']['arp_inspection']):
             snooping_vlans = util.intify(global_params['ip']['arp_inspection']['vlans'])
     else:
-        result_dict['ip']['ARP inspection'] = [0, 'DISABLED', 'Turn it on to prevent spoofing attack']
+        result_dict['IP options']['ARP inspection'] = [0, 'DISABLED', 'Turn it on to prevent spoofing attack']
     return result_dict, enabled
