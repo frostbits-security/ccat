f578e0f6dce71296dbe1531b8cbae9ca3e298755
khodunken <khodukinant75@gmail.com>
2018-08-17 21:09:05 +0300

    upgrade vtp check

--- checks/vtp.py
+++ checks/vtp.py
@@ -5,4 +5,6 @@ def check(global_dct):
             result['VLAN Trunking Protocol(VTP) mode']=[3,global_dct['vtp']['mode'][0].capitalize(),'VTP should be turned off']
         elif 'domain' in global_dct['vtp']:
             result['VLAN Trunking Protocol(VTP) mode'] = [1, 'Server/Client', 'VTP should be turned off']
-        return {'VLAN Trunking Protocol':result}
+    else:
+        result['VLAN Trunking Protocol(VTP)'] = [2, 'DISABLE', 'VTP should be turned off']
+    return {'VLAN Trunking Protocol':result}
