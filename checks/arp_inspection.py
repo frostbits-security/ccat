23264fe6f65773e6783c6ff0ef0b6bcf406b1212
khodunken <khodukinant75@gmail.com>
2018-08-17 13:29:54 +0300

    fixed bugs in arp_inspection check

--- checks/arp_inspection.py
+++ checks/arp_inspection.py
@@ -2,34 +2,31 @@
 # ARP inspection check
 
 def check(iface_params, vlanmap, allinterf, enabled):
-    ### Interfaces
-    if (enabled):
+	### Interfaces
+	result_dict = {}
+	if (enabled):
+		try:
+			iface_arp = iface_params['arp_insp']
+		except:
+			pass
+		iface_vlans = []
+		# check if interface has vlans on it
+		if 'vlans' in iface_params:
+			iface_vlans = iface_params['vlans']
+		# check if interface is turned off, and if we need to check disabled interfaces
+		if (not (allinterf) and iface_params['shutdown'] == 'yes'):
+			pass
+		else:
+			# create dictionary for output
+			if not ('ARP inspection' in result_dict):
+				result_dict['ARP inspection'] = {}
+			mode = iface_arp['mode']
 
-        # create dictionary for output
-        result_dict = {}
-        # check if dhcp snooping is active on interface
-        try:
-            iface_arp = iface_params['arp_insp']
-        except:
-            pass
-        iface_vlans = []
-        # check if interface has vlans on it
-        if 'vlans' in iface_params:
-            iface_vlans = iface_params['vlans']
-        # check if interface is turned off, and if we need to check disabled interfaces
-        if (not (allinterf) and iface_params['shutdown'] == 'yes'):
-            pass
-        else:
-            # create dictionary for output
-            if not ('ARP inspection' in result_dict):
-                result_dict[i]['ARP inspection'] = {}
-            mode = iface_arp['mode']
-
-            if ((mode == 'trust') and vlanmap and iface_vlans):
-                if (set(vlanmap[2]).isdisjoint(iface_vlans)):
-                    result_dict[i]['ARP inspection']['vlans'] = [0,
+			if ((mode == 'trust') and vlanmap and iface_vlans):
+				if (set(vlanmap[2]).isdisjoint(iface_vlans)):
+					result_dict[i]['ARP inspection']['vlans'] = [0,
                                                                  'Interface set as trusted, but vlanmap is different',
                                                                  'This interface is not trusted according to vlanmap, but marked as trusted. ARP spoofing is possible.']
-            else:
-                result_dict = 0
-    return result_dict
+			else:
+				result_dict = 0
+	return result_dict
