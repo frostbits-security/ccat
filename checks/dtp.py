d643b97bee9020b734688d8fb8b440bb6584c533
nepJIywa <nikitaloginov96@mail.ru>
2018-10-18 21:09:55 +0300

    Fixed dtp comments

--- checks/dtp.py
+++ checks/dtp.py
@@ -23,11 +23,11 @@ def dtp_check(global_dct,iface_dct,result,scale):
 def check(global_dct,iface_dct, vlanmap_type):
     result = {}
 
-# If this network segment is TRUSTED - enabled cdp is not a red type of threat, it will be colored in orange
+# If this network segment is TRUSTED - enabled DTP is not a red type of threat, it will be colored in orange
     if vlanmap_type == 'TRUSTED':
         dtp_check(global_dct,iface_dct,result,[1,2])
 
-# Otherwise if network segment is CRITICAL or UNKNOWN or vlanmap is not defined - enabled cdp is a red type of threat
+# Otherwise if network segment is CRITICAL or UNKNOWN or vlanmap is not defined - enabled DTP is a red type of threat
     else:
         dtp_check(global_dct,iface_dct,result, [0, 2])
 
