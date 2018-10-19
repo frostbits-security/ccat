f3a6c135e88bf243efa8526edc20b5ca5dc41711
khodunken <khodukinant75@gmail.com>
2018-10-19 22:23:58 +0300

    fixed logic mistake

--- checks/vtp.py
+++ checks/vtp.py
@@ -4,7 +4,7 @@ def check(global_dct):
         if 'mode' in global_dct['vtp']:
             result['VLAN Trunking Protocol(VTP) mode']=[3,global_dct['vtp']['mode'][0].capitalize(),'VTP should be turned off']
         elif 'domain' in global_dct['vtp']:
-            result['VLAN Trunking Protocol(VTP) mode'] = [1, 'Server/Client', 'VTP should be turned off']
-    else:
-        result['VLAN Trunking Protocol(VTP)'] = [2, 'DISABLED']
+            result['VLAN Trunking Protocol(VTP) mode'] = [1, 'Server/Client mode', 'VTP should be turned off']
+        else:
+            result['VLAN Trunking Protocol(VTP) mode'] = [1, 'Server/Client mode','VTP should be turned off']
     return {'VLAN Trunking Protocol':result}
