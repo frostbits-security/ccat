9d0ba074104b762d267754755ce9f607d1d0340f
Nikita <nikitaloginov96@mail.ru>
2018-11-07 01:43:02 +0300

    Improved graph's everything, debug output is still active

--- graph.py
+++ graph.py
@@ -1,86 +1,78 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+def draw_plot(switches_dict, vlanmap=False):
 
-def draw_plot(switches_dict):
-
+    # Fill list Sets in the next format: [(vlan_num, 'dev_name_int'),(...)]
     sets = []
-    sets2 = []
-
     for switch in switches_dict:
         result_dict = {}
-
         for iface in switches_dict[switch]:
             if len(switches_dict[switch][iface]) > 0:
-                result_dict.update({iface:switches_dict[switch][iface]})
-        print(result_dict)
-
+                result_dict.update({iface: switches_dict[switch][iface]})
         for iface_vlans in result_dict:
             for vlan in result_dict[iface_vlans]:
                 sets.append((vlan, switch + ' ' + iface_vlans))
-    print('..............FULL....SETS.............')
-    print(sets)
 
-    # Filling set1 as RED nodes on graph and set2 as BLUE nodes: LIKE PAIR OF DEVICE NAME + INTERFACE NAME
-    # and ITS VLANS [('dev_name_int_name', [4,5,6),('dev_name_int_name2', [4,5,6,7,8)]
-    sets1 = sets.copy()
-    x = 0
-    for xxx in sets:
-        got_it = False
-        x += 1
-        fff = x
-        while fff < len(sets):
-            if xxx[0] == sets[fff][0]:
-                sets2.append(sets[fff])
-                sets1.remove(sets[fff])
-                got_it = True
-            fff += 1
-        if got_it:
-            sets2.append(sets[sets.index(xxx)])
-            sets1.remove(sets[sets.index(xxx)])
+    # Fill 2 nodes lists to color them in different colors on graph
+    central_nodes_list = []
+    edge_nodes_list = []
+    for node_pair in sets:
+        found = False
+        if node_pair[0] in central_nodes_list:
+            continue
+        for next_node_pair in sets:
+            if node_pair == next_node_pair:
+                continue
+            if node_pair[0] == next_node_pair[0]:
+                central_nodes_list.append(node_pair[0])
+                found = True
+                break
+        if found is False:
+            edge_nodes_list.append(node_pair[0])
 
-    print('..................SETS1.............')
-    print(sets1)
-    print('..................SETS2.............')
-    print(sets2)
+    # Debug output
+    print('..............CENTRAL NODES LIST.............')
+    print(central_nodes_list)
+    print('..............EDGE NODES LIST.............')
+    print(edge_nodes_list)
+    print('--------------IN CASE IF WE HAVE SAME VLANS IN 2 LISTS THE LIST BELOW WONT BE EMPTY--------------')
+    print([i for i in central_nodes_list if i in edge_nodes_list])
+
+    # # For PDF format, otherwise internal error will occur
+    # plt.rcParams['pdf.fonttype'] = 42
+    # plt.rcParams['font.family'] = 'Calibri'
 
     # draw graph
     G = nx.Graph()
     G.add_edges_from(sets)
     pos = nx.spring_layout(G)
 
-    print('..............................G NODES......................')
-    print(G.nodes)
-    print('..............................G EDGES......................')
-    print(G.edges)
+    # Increase plot size
+    plt.figure(figsize=(16,9))
 
-    # lst1 - list of sets1 nodes, lst2 - list of sets2 nodes: LIKE VLAN NUMBERS [1,4,6,7]
-    lst1 = []
-    for item in sets1:
-        lst1.append(item[0])
-    lst2 = []
-    for item in sets2:
-        lst2.append(item[0])
+    # Draw nodes
+    legend_edge_node = nx.draw_networkx_nodes(G, pos, nodelist=edge_nodes_list,    node_color="#ff4d4d", with_labels=False)
+    legend_cent_node = nx.draw_networkx_nodes(G, pos, nodelist=central_nodes_list, node_color="#4d4dff", with_labels=False)
 
-    print('---------------------------LIST 1 AND LIST 2 --------------------')
-    print(lst1)
-    print(lst2)
-    print('------------------------------POS--------------------------------')
-    print(pos)
+    # Draw edges (connections)
+    legend_conn_int  = nx.draw_networkx_edges(G, pos, edge_color='black', font_size=16, with_labels=False)
 
-    nx.draw_networkx_nodes(G, pos, nodelist=lst1, node_color="r", with_labels=False)
-    nx.draw_networkx_nodes(G, pos, nodelist=lst2, node_color="b", with_labels=False)
+    # Draw labels
+    nx.draw_networkx_labels(G, pos, font_size=10)
 
-    nx.draw_networkx_edges(G, pos, edge_color='black', font_size=16, with_labels=False)
+    # Delete X and Y symbols and values on graph
+    plt.xticks([])
+    plt.yticks([])
 
+    # Plot's legend
+    plt.legend((legend_edge_node, legend_cent_node, legend_conn_int), ('Single vlans', 'Multiple vlans', 'Connected interface'), loc=1, framealpha=0.5, fontsize='small', markerscale=0.5,edgecolor='Black')
 
-    # raise text positions
-    for p in pos:
-        pos[p][1] += 0.07
-    nx.draw_networkx_labels(G, pos)
+    # Delete space between plot and window edge
+    plt.tight_layout()
 
     # Save graph as png picture
-    #plt.savefig("network_map.png")
+    plt.savefig("network_map.png", dpi=1000)
 
+    # Interactive mode
     plt.show()
-
