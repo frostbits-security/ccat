2c00d55ee7857b5486577891e619d399d7c1fbf9
AetherEternity <swing056@gmail.com>
2018-11-13 14:13:55 +0300

    Fixed harvester and integrated it into CCAT

--- harvester.py
+++ harvester.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
+####
+# Credentials harvesting module
 import re
-import json
 import os
-import sys
+
+# Legacy code from early stages
+"""import sys
 
 try:
 	directory=sys.argv[1]+'/'
 	filelist=os.listdir(directory)
 except:
 	print("Usage: ./harvester.py /path/to/dir/with/configs")
-	exit()
+	exit()"""
 
-reg_pass=r"enable password (\d).+\b([\w\$]+)"
-reg_secret=r"enable secret (\d).+\b([\w\$]+)"
+reg_pass=r"enable password (\d).+\b([\w$\\]+)"
+reg_secret=r"enable secret (\d).+([\w\$\\]{30,43})"
 reg_username_pass=r"username ([\w]+).+password (\d) \b(.+)"
-reg_username_secret=r"username ([\w]+).+secret (\d) \b(.+)"
+reg_username_secret=r"username ([\w]+).+secret (\d).+([\w\$\\]{30,43})"
 
 usernames=[]
 passwords=[]
@@ -58,6 +61,7 @@ def parseusersecret(content):
 			secrets.append(tuple([result[1],result[2]]))
 			usernames.append(result[0])
 
+#### Type 7 decoding
 #from https://github.com/theevilbit/ciscot7
 xlat = [0x64, 0x73, 0x66, 0x64, 0x3b, 0x6b, 0x66, 0x6f, 0x41, 0x2c, 0x2e, 0x69, 0x79, 0x65, 0x77, 0x72, 0x6b, 0x6c, 0x64
 , 0x4a, 0x4b, 0x44, 0x48, 0x53, 0x55, 0x42, 0x73, 0x67, 0x76, 0x63, 0x61, 0x36, 0x39, 0x38, 0x33, 0x34, 0x6e, 0x63,
@@ -82,36 +86,49 @@ def decrypt_type7(ep):
 		dp += newchar
 	return dp
 
-
-
-
-
-for file in filelist:
-	with open(directory+file) as infile:
-		try:
-			tmp=infile.read()
+def harvest(filelist):
+	global reg_pass
+	global reg_secret
+	global reg_username_pass
+	global reg_username_secret
+	global usernames
+	global passwords
+	global secrets
+	global clears
+	
+	for file in filelist:
+		with open(file) as infile:
+			try:
+				tmp=infile.read()
+			except:
+				continue
+			parseusersecret(tmp)
+			parseuserpass(tmp)
+			parsesecret(tmp)
+			parsepass(tmp)
+	
+	passwords=list(set(passwords))
+	for passw in passwords:
+		if(passw[0]=="0"):
+			clears.append(passw[1])
+			continue
+		try:	
+			clears.append(decrypt_type7(passw[1]))
 		except:
 			continue
-		parseusersecret(tmp)
-		parseuserpass(tmp)
-		parsesecret(tmp)
-		parsepass(tmp)
-
-passwords=list(set(passwords))
-for passw in passwords:
-	if(passw[0]=="0"):
-		clears.append(passw[1])
-		continue
-	try:	
-		clears.append(decrypt_type7(passw[1]))
-	except:
-		continue
-	
-with open('usernames.json', 'w') as outfile:
-	json.dump(list(set(usernames)), outfile)
-with open('passwords.json', 'w') as outfile:
-	json.dump(passwords, outfile)
-with open('secrets.json', 'w') as outfile:
-	json.dump(list(set(secrets)), outfile)
-with open('clears.json', 'w') as outfile:
-	json.dump(clears, outfile)
+		
+	with open('dumped_usernames.txt', 'w') as outfile:
+		for username in list(set(usernames)):
+			outfile.write("%s\n" % username)
+	with open('dumped_passwords.txt', 'w') as outfile:
+		for password in list(set(clears)):
+			outfile.write("%s\n" % password)
+	m500=open('dumped_hashes_m500.txt', 'w')
+	m5700=open('dumped_hashes_m5700.txt', 'w')
+	for secret in list(set(secrets)):
+		if secret[0]=="5":
+			m500.write("%s\n" % secret[1])
+		elif secret[0]=="4":
+			m5700.write("%s\n" % secret[1])
+	m500.close()
+	m5700.close()
